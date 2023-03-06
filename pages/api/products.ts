dacaa3c416e9a6250ce6f1e72850c260b334792f
Aydin <ms1166760@gmail.com>
2023-03-06 23:29:33 +0330

    Connected to MongoDB database

---
+++ pages/api/products.ts
@@ -0,0 +1,20 @@
+import CollectionDB from "@/db/mongoDB";
+import { NextApiHandler } from "next";
+import { MongoClient, ServerApiVersion } from "mongodb"
+
+
+const Handler: NextApiHandler = async (req, res) => {
+  if (req.method === "GET") {
+    let collection = await CollectionDB('products')
+    if (!collection) {
+      res.status(500).json({ message: "We could not connect to the database!" })
+      return
+    }
+    let products = await collection.find({}).toArray()
+    res.status(200).json({ products })
+  } else {
+    res.status(400).json({ message: "method is false" })
+  }
+}
+
+export default Handler
