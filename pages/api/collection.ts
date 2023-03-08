9df259b46b9ce4038138f5514a3d178ff9c68a52
Aydin <ms1166760@gmail.com>
2023-03-09 00:24:34 +0330

    Add api/get-info and collection

--- pages/api/collection.ts
+++ pages/api/collection.ts
@@ -3,13 +3,13 @@ import { NextApiHandler } from "next";
 
 const Handler: NextApiHandler = async (req, res) => {
   if (req.method === "GET") {
-    let collection = await CollectionDB('products')
+    let collection = await CollectionDB('collection')
     if (!collection) {
       res.status(500).json({ message: "We could not connect to the database!" })
       return
     }
-    let products = await collection.find({}).toArray()
-    res.status(200).json({ products })
+    let allCollection = await collection.find({}).toArray()
+    res.status(200).json({ collection: allCollection })
   } else {
     res.status(400).json({ message: "method is false" })
   }
---
+++ pages/api/get-info.ts
@@ -0,0 +1,36 @@
+import CollectionDB from "@/db/mongoDB";
+import { NextApiHandler } from "next";
+
+const Handler: NextApiHandler = async (req, res) => {
+  if (req.method === "GET") {
+    const { token } = req.headers
+    if (!token) {
+      res.status(400).json({ message: "There is no token in the header" })
+      return
+    }
+
+    let collectionToken = await CollectionDB('users-token')
+    let collectionInfo = await CollectionDB('users-info')
+
+    let userToken = await collectionToken.find({ token }).toArray()
+    let userInfo = await collectionInfo.find({ token }).toArray()
+
+    if (userToken[0] && userInfo[0]) {
+      let user = {
+        ...userInfo[0],
+        profile: {
+          ...userInfo[0].profile,
+          email: userToken[0].email
+        }
+      }
+      res.status(200).json({ message: "Success", user })
+    } else {
+      res.status(404).json({ message: "not found account!" })
+    }
+
+  } else {
+    res.status(400).json({ message: "method is false" })
+  }
+}
+
+export default Handler;
